0d76652aec519ab4fc427915ee3c57b871a554cf
Pascal de Vink <pascal.de.vink@gmail.com>
2014-08-26 18:22:11 +0200

    Added a recall-theme with Recall specific styling. Also added some dummy data as that's easier.

--- src/Wecamp/Recall/Controller/TimelineController.php
+++ src/Wecamp/Recall/Controller/TimelineController.php
@@ -8,6 +8,25 @@ class TimelineController
 
     public function listAction()
     {
-        return $this->getTemplate()->render('timeline.html.twig');
+        return $this->getTemplate()->render('timeline.html.twig', array(
+            'name' => 'Fred',
+            'gender' => 'Male',
+            'age' => 26,
+            'professionalGroup' => 'Tester at Big Technical Corp.',
+            'timeline' => array(
+                array(
+                    'timestamp' => 1409068419,
+                    'description' => 'Bob added a recipe',
+                ),
+                array(
+                    'timestamp' => 1409068419,
+                    'description' => 'Alice verified a recipe',
+                ),
+                array(
+                    'timestamp' => 1409068419,
+                    'description' => 'Alice pulled a recipe',
+                ),
+            )
+        ));
     }
 }

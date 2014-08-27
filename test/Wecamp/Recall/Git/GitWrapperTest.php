683a9a10d6ba9d2f878212135d3a078ff4f98e54
Pascal de Vink <pascal.de.vink@gmail.com>
2014-08-27 11:42:12 +0200

    Added until test for the RecallGitWrapper

--- test/Wecamp/Recall/Git/GitWrapperTest.php
+++ test/Wecamp/Recall/Git/GitWrapperTest.php
@@ -2,10 +2,87 @@
 
 namespace Wecamp\Recall\Git;
 
+use GitWrapper\GitWorkingCopy;
+use \Mockery as m;
+
 class GitWrapperTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTrue()
+    protected function tearDown() {
+        m::close();
+    }
+
+    public function testCallPassesTheMethod()
     {
-        $this->assertTrue(true);
+        $gitWrapper = m::mock('GitWrapper\GitWrapper');
+        $gitWrapper->shouldReceive('init')->times(1)->with('test')->andReturn(true);
+
+        $recallGitWrapper = new GitWrapper($gitWrapper);
+
+        $returnValue = $recallGitWrapper->init('test');
+        $this->assertTrue($returnValue);
+    }
+
+    /**
+     * @expectedException \BadMethodCallException
+     * @expectedExceptionMessage Unknown method arnold()
+     */
+    public function testCallWithBadMethod()
+    {
+        $gitWrapper = m::mock('GitWrapper\GitWrapper');
+        $recallGitWrapper = new GitWrapper($gitWrapper);
+
+        $recallGitWrapper->arnold();
+    }
+
+    public function testCallReturnsGitWorkingCopyInstance()
+    {
+        $gitWrapper = m::mock('GitWrapper\GitWrapper');
+        $gitWorkingCopy = new GitWorkingCopy($gitWrapper, 'data');
+        $gitWrapper->shouldReceive('init')->times(1)->andReturn($gitWorkingCopy);
+        $gitWrapper->shouldReceive('run')->times(2);
+
+        $recallGitWrapper = new GitWrapper($gitWrapper);
+
+        $returnValue = $recallGitWrapper->init();
+        $this->assertSame($recallGitWrapper, $returnValue);
+    }
+
+    public function testCallReturnsGitWrapperInstance()
+    {
+        $gitWrapper = m::mock('GitWrapper\GitWrapper');
+        $gitWrapper->shouldReceive('init')->times(1)->andReturn($gitWrapper);
+
+        $recallGitWrapper = new GitWrapper($gitWrapper);
+
+        $returnValue = $recallGitWrapper->init();
+        $this->assertSame($recallGitWrapper, $returnValue);
+    }
+
+    /**
+     * @expectedException \BadMethodCallException
+     * @expectedExceptionMessage Unknown method add()
+     */
+    public function testCallWithYetUndefinedMethod()
+    {
+        $gitWrapper = m::mock('GitWrapper\GitWrapper');
+        $recallGitWrapper = new GitWrapper($gitWrapper);
+
+        $recallGitWrapper->add(false);
+    }
+
+    public function testCallWithDefinedMethodAfterGitWorkingCopyReturned()
+    {
+        $gitWrapper = m::mock('GitWrapper\GitWrapper');
+        $gitWorkingCopy = m::mock('GitWrapper\GitWorkingCopy');
+        $gitWorkingCopy->shouldReceive('config')->times(2);
+        $gitWrapper->shouldReceive('init')->times(1)->andReturn($gitWorkingCopy);
+        $gitWorkingCopy->shouldReceive('add')->times(1)->andReturn($gitWrapper);
+
+        $recallGitWrapper = new GitWrapper($gitWrapper);
+        $recallGitWrapper->init(false);
+
+        $returnValue = $recallGitWrapper->add(false);
+
+        $this->assertSame($recallGitWrapper, $returnValue);
     }
 }

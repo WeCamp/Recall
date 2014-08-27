efca36ef6c638b4b06c186b180ab99308d243649
Pascal de Vink <pascal.de.vink@gmail.com>
2014-08-27 10:03:44 +0200

    Added PHPUnit and a first unit test

---
+++ test/Wecamp/Recall/Git/GitWrapperTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wecamp\Recall\Git;
+
+class GitWrapperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}

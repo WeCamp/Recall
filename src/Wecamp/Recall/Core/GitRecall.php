953699c778933da5444b1b27cd2f0b0234c67e2b
Jasper N. Brouwer <jasper@nerdsweide.nl>
2014-08-29 01:03:22 +0200

    Clear output before grabbing the git log

--- src/Wecamp/Recall/Core/GitRecall.php
+++ src/Wecamp/Recall/Core/GitRecall.php
@@ -169,12 +169,14 @@ class GitRecall implements Recallable
      */
     private function readLog(Context $context = null)
     {
+        $this->gitWrapper->clearOutput();
+
         if ($context) {
             $this->gitWrapper->log('--name-status', $context->getName());
         } else {
             $this->gitWrapper->log('--name-status');
         }
-        
+
         return $this->gitWrapper->getOutput();
     }
 
@@ -184,7 +186,10 @@ class GitRecall implements Recallable
      */
     private function readLogStats(Context $context = null)
     {
+        $this->gitWrapper->clearOutput();
+
         $this->gitWrapper->log('--stat');
+
         return $this->gitWrapper->getOutput();
     }
 
